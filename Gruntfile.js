20a862090230667d8ec4281518bb46e7d5caec76
Theodore Brown <theodorejb@outlook.com>
2014-03-15 21:47:08 -0500

    Update dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
                     sourceMap: true,
                     declaration: false,
                     removeComments: false
-                },
+                }
             }
         },
         browserify: {
