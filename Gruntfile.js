5cc944919380ba8e6d3a0834d65a830923b6514e
Theodore Brown <theodorejb@outlook.com>
2014-07-30 21:26:26 -0500

    Simplify Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,34 +7,25 @@ module.exports = function (grunt) {
             pub: {
                 src: ['lib/*.ts', 'test/*.ts'],
                 options: {
-                    module: 'commonjs',   // 'amd' (default) | 'commonjs'
-                    sourceMap: false,      // true (default) | false
-                    declaration: true,    // generate a declaration .d.ts file for every output js file. [true | false (default)]
-                    removeComments: false // true (default) | false
+                    module: 'commonjs',    // use commonjs instead of amd
+                    sourceMap: true,       // useful for debugging
+                    declaration: true,     // generate a declaration .d.ts file for every output js file
+                    removeComments: false,
                 },
             },
-            dev: {
-                src: ['lib/*.ts', 'test/*.ts'],
-                options: {
-                    module: 'commonjs',
-                    sourceMap: true,
-                    declaration: false,
-                    removeComments: false
-                }
-            }
         },
         browserify: {
             dev: {
                 src: ['test/*.js'],
-                dest: 'test/browser/testbundle.js'
-            }
-        }
+                dest: 'test/browser/testbundle.js',
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-browserify');
 
     // see http://gruntjs.com/creating-tasks
-    grunt.registerTask('default', ['ts:dev', 'browserify:dev']);
+    grunt.registerTask('default', ['ts:pub', 'browserify:dev']);
     grunt.registerTask('pub', ['ts:pub', 'browserify:dev']);
 };
